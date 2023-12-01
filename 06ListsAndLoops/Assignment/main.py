1fef950ff5db00fce41dae1f9b4acf7bbcca9492
Kyler Sevart <26kylersevart@usd266.com>
2023-12-01 10:16:32 -0600

    implement 3 functions

--- 06ListsAndLoops/Assignment/main.py
+++ 06ListsAndLoops/Assignment/main.py
@@ -57,3 +57,44 @@ inputlist = [-1, 100, 35, 30, 25, 55, 104]
 returnvalue = average_act_score(inputlist)
 print("This is the average ACT score")
 print(returnvalue)
+
+
+def all_true(list):
+    for boolean in list:
+    return True
+
+print("Demonstrate all_true")
+print("[true, true, false] = ", all_true([True, True, False]))
+print("[true, true, true] = ", all_true([True, True, True]))
+
+def any_true(list):
+    for boolean in list:
+        if boolean in [True]:
+            return True
+    return False
+
+print("Demonstrate any_true")
+print("[false, false, true] = ", any_true([False, False, True]))
+print("[false, false, false] = ", any_true([False, False, False]))
+
+def mostly_true(list):
+    word1, word2, word3 = list
+
+    word1 = list[0]
+    word2 = list[1]
+    word3 = list[2]
+
+    if word1 == True and word2 == True and word3 == False:
+        return True
+    elif word1 == True and word2 == False and word3 == True:
+        return True
+    elif word1 == False and word2 == False and word3 == True:
+        return False
+    elif word1 == False and word2 == True and word3 == True:
+        return True
+    elif word1 == False and word2 == True and word3 == False:
+        return False
+    
+print("Demonstrate mostly_true")
+print("[true, true, false] = ", mostly_true([True, True, False]))
+print("[false, false, true] = ", mostly_true([False, False, True]))
