de63d900075c28b672a2365580a514bad5d63ab6
Kyler Sevart <26kylersevart@usd266.com>
2023-12-01 09:29:00 -0600

    changes

---
+++ 06ListsAndLoops/Assignment/main.py
@@ -0,0 +1,57 @@
+def count_failing_grades(list):
+    count = 0
+    for numbers in list:
+        if numbers in ["60%", "55%", "50%", "45%", "40%", "35%", "30%", "25%", "20%", "15%", "10%"]:
+            count = count + 1
+
+
+    return count
+
+inputlist = ["60%", "55%", "50%", "80%", "65%"]
+returnvalue = count_failing_grades(inputlist)
+print("This many people have failing grades")
+print(returnvalue)
+
+
+def count_act_score(list):
+    count = 0
+    for numbers in list:
+        if numbers in ["65%", "70%", "75%", "80%", "85%", "90%", "95%", "100%"]:
+            count = count + 1
+
+
+    return count
+
+inputlist = ["75%", "80%", "55%", "65%", "10%", "90%", "95%", "100%", "50%", "60%"]
+returnvalue = count_act_score(inputlist)
+print("This many people have a valid ACT score")
+print(returnvalue)
+
+
+def number_sum(numbers):
+    total = 0
+    for number in numbers:
+        total = total + number
+
+    return total
+
+inputlist = [4, 3, 6, 9]
+returnvalue = number_sum(inputlist)
+print("This is the total")
+print(returnvalue)
+
+
+def average_act_score(list):
+    total = 0
+    for numbers in list:
+        if numbers in [10, 9, 8, 7, 6]:
+            total = total + numbers
+        
+    return total / len(list)
+    
+
+
+inputlist = [5, 7, 6, 3, 3]
+returnvalue = average_act_score(inputlist)
+print("This is the average ACT score")
+print(returnvalue, "%")
