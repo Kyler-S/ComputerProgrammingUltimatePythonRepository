ba7252ac1fac57afd1c115e03aae4ad6380d215c
Kyler Sevart <26kylersevart@usd266.com>
2023-12-11 09:16:26 -0600

    changes

--- 07StringsAsLists/Assignment/main.py
+++ 07StringsAsLists/Assignment/main.py
@@ -19,12 +19,63 @@ print("Demonstrate count_vowels")
 print("apple = ", count_vowels("apple"))
 print("elephant = ", count_vowels("elephant"))
 
-def count_numbers(list):
+def count_numbers(string):
     count = 0
-    for numbers in list:
-        if numbers > 1:
-            count = count + numbers
+    for character in string:
+        if character in "123456789":
+            count = count + 1
     return count
 
 print("Demonstrate count_numbers")
 print("789ghi = ", count_numbers("789ghi"))
+print("123abc = ", count_numbers("123abc"))
+
+def count_target_numbers(word1, word2):
+    count = 0
+    for letter in word1:
+        if letter == word2:
+            count = count + 1
+    return count
+
+print("Demonstrate count_target_numbers")
+print("apple, a = ", count_target_numbers("apple", "a"))
+print("banana, a = ", count_target_numbers("banana", "a"))
+
+
+def in_alphabetical_order(string):
+    previous = "a"
+    for letter in string:
+        if letter >= previous and letter <= previous:
+            return False
+    return True
+
+
+print("Demonstrate in_alphabetical_order")
+# bin -> true
+# apple -> false
+print(in_alphabetical_order("bin"))
+print(in_alphabetical_order("apple"))
+print(in_alphabetical_order("abc"))
+
+def alternate_case(string):
+    result = ""
+    next_upper = True
+    for letter in string:
+        if letter == string:
+            next_upper = True
+        elif next_upper == True:
+            letter.upper()
+            next_upper = False
+        elif next_upper == False:
+            next_upper == True
+        elif next_upper == True:
+            letter.upper()
+            next_upper = False
+        elif next_upper == False:
+             next_upper == True
+        elif next_upper == True:
+            letter.upper()
+            next_upper = False
+
+print("Demonstrate alternate_case")
+print(alternate_case("python"))
