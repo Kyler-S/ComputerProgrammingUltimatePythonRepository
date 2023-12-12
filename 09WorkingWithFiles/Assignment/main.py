58d0b47c3e3644d7411e8ae509f112af82765daa
Kyler Sevart <26kylersevart@usd266.com>
2023-12-12 10:09:53 -0600

    started work

--- 09WorkingWithFiles/Assignment/main.py
+++ 09WorkingWithFiles/Assignment/main.py
@@ -1,1 +1,13 @@
 import csv
+f = open("../data/4000-most-common-english-words.csv", "r")
+words = f.read().split("\n")
+f.close()
+print("You on 4000-most-common-english-words.csv")
+def english_most_common_english_words_csv(file):
+    most_vowel_word = ""
+    average_word_length = 0
+    common_starting_letter = ""
+    for vowels in file:
+        if vowels in ["a", "e", "i", "o", "u"]:
+            most_vowel_word = vowels
+        return most_vowel_word
